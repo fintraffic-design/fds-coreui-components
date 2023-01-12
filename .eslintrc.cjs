9f64e265c0144375b125b41f98ade9deb9221fe0
Nino Penttinen <nino.penttinen@solita.fi>
2023-01-12 15:33:22 +0200

    Ignore rules conflicting with prettier in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'standard-with-typescript',
+  extends: ['standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
     project: ['./tsconfig.json'],
@@ -12,12 +12,10 @@ module.exports = {
   },
   rules: {
     semi: 'off',
-    indent: ['error', 2],
     'space-in-parens': 'off',
     'no-trailing-spaces': ['error', { skipBlankLines: true }],
     'comma-dangle': 'off',
     '@typescript-eslint/comma-dangle': 'off',
-    '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/space-before-function-paren': ['error', { anonymous: 'always', named: 'never' }],
     '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',

67ec7f63a15b67fe7cb6543d4e459acb90c5a96f
Markus Pulkkinen <markus.pulkkinen@solita.fi>
2023-01-18 17:36:43 +0200

    fds-icon refactoring: icon & color types from dependencies, events can be attached to :host element

--- src/fds-icon.ts
+++ src/fds-icon.ts
@@ -1,71 +1,45 @@
-import { LitElement } from 'lit'
+import { css, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import { AlertCircle, AlertTriangle, ChevronRight, createElement, PlusCircle, Trash2 } from 'lucide'
-
-const IconTypeMap = {
-  'chevron-right': ChevronRight,
-  'alert-triangle': AlertTriangle,
-  'alert-circle': AlertCircle,
-  'plus-circle': PlusCircle,
-  'trash-2': Trash2,
-}
-
-const ColorMap = {
-  black: '#000000',
-  gray: '#9696AA',
-  interactive: '#1777F8',
-  danger: '#B40000',
-}
+import * as lucide from 'lucide'
+import { Color, token } from './token-utils'
 
 type SvgSize = '18' | '24' | '36'
 
 /**
  * Add interactible icon element. Icon library: https://lucide.dev/
  *
- * @property {string} icon - Options:
- * - alert-circle
- * - alert-triangle
- * - chevron-right
- * - plus-circle
- * - trash-2
+ * @property {string} icon
+ * Any lucide icon name in PascalCase eg: AlertCircle
  * @property {string} size - Options:
  * - 18
  * - 24 (default)
  * - 36
  * @property {string} color - Options:
- * - black (default)
- * - danger
- * - gray
- * - interactive
- * @property {string} class - CSS style class.
- * @property {(e: MouseEvent) => void} onClick - On click handler for the icon.
+ * Any fds design token color value.
+ * Default: color-text-1000
  */
 @customElement('fds-icon')
 export default class FdsIcon extends LitElement {
+  static override styles = css`
+    :host {
+      display: inline-flex;
+    }
+  `
+
   @property() size: SvgSize = '24'
-  @property() color: keyof typeof ColorMap = 'black'
-  @property() icon?: keyof typeof IconTypeMap
-  @property() class?: string
-  @property() onClick?: (e: MouseEvent) => void
+  @property() color: Color = 'color-text-1000'
+  @property() icon: keyof typeof lucide.icons = 'AlertCircle'
 
   override render(): SVGElement | null {
-    if (!this.icon) {
+    const lucideIcon = lucide[this.icon]
+    if (lucideIcon == null) {
+      console.error('invalid icon', this.icon)
       return null
     }
-
-    const svgElement = createElement(IconTypeMap[this.icon])
-    svgElement.setAttribute('color', ColorMap[this.color])
+    const svgElement = lucide.createElement(lucideIcon)
+    svgElement.setAttribute('color', token(this.color).cssText)
     svgElement.setAttribute('width', this.size)
     svgElement.setAttribute('height', this.size)
-
-    if (this.class) {
-      svgElement.classList.add(this.class)
-    }
-
-    if (this.onClick) {
-      svgElement.addEventListener('click', this.onClick)
-    }
-
     return svgElement
   }
 }
--- src/token-utils.ts
+++ src/token-utils.ts
@@ -1,4 +1,11 @@
 import { CSSResult, unsafeCSS } from 'lit'
 import { tokens } from '@fintraffic-design/coreui-css/dist/tokens'
 
-export const token: (key: keyof typeof tokens) => CSSResult = key => unsafeCSS(tokens[key])
+export const token: (key: keyof typeof tokens) => CSSResult = key => {
+  const value = tokens[key]
+  if (value == null) {
+    console.error('invalid token', key)
+  }
+  return unsafeCSS(value)
+}
+export type Color = keyof typeof tokens & `color-${string}`

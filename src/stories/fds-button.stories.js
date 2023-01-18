30b06441a51d7ebefc1221f11afb3e2a407d2bbf
Nino Penttinen <nino.penttinen@solita.fi>
2023-01-18 15:49:08 +0200

    AT-34 Add stories for button

---
+++ src/stories/fds-button.stories.js
@@ -0,0 +1,24 @@
+import { html } from 'lit'
+
+import '../fds-button'
+
+export default {
+  title: 'Button',
+}
+
+const Template = ({ variant, disabled }) =>
+  html`<fds-button .variant=${variant} .disabled=${disabled}>Text</fds-button>`
+
+export const Enabled = Template.bind({})
+
+Enabled.args = {
+  variant: 'primary',
+  disabled: false,
+}
+
+export const Disabled = Template.bind({})
+
+Disabled.args = {
+  variant: 'primary',
+  disabled: true,
+}

6bf92644ee3c659d99d3a676b2853f5a69009ce9
Markus Pulkkinen <markus.pulkkinen@solita.fi>
2023-01-24 13:30:05 +0200

    AT-28 button

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|mjs|ts)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|mjs)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/web-components',
   core: {
-    disableTelemetry: true,
+    builder: '@storybook/builder-vite',
   },
 }

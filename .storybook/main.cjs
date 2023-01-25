c6eb49afee2ebf6314b9145a646af31ce4e0edb7
Markus Pulkkinen <markus.pulkkinen@solita.fi>
2023-01-25 09:51:27 +0200

    fix storybook-build base path

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,8 +1,13 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|mjs)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/web-components',
   core: {
     builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, { base: "/coreui-components/" })
   },
 }

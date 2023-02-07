626996a6efe717950730bcdb6df9e20e1800985e
Nino Penttinen <nino.penttinen@solita.fi>
2023-02-07 15:31:46 +0200

    AT-47 Disable keyboard shortcuts (see desc.)
    Keyboard shortcuts would get triggered even when writing on a input in a component...

--- .storybook/preview.mjs
+++ .storybook/preview.mjs
@@ -1,1 +1,8 @@
 import '@fintraffic-design/coreui-css/dist/coreui.css'
+import { addParameters } from '@storybook/web-components'
+
+addParameters({
+  options: {
+    enableShortcuts: false,
+  },
+})

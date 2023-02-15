1749b122f3b951f4bb43651b33d22c89352ac150
Nino Penttinen <nino.penttinen@solita.fi>
2023-02-15 17:08:18 +0200

    Remove obsolete testing libraries

--- src/fds-card.test.ts
+++
@@ -1,9 +0,0 @@
-import { FdsCard } from './fds-card'
-import { assert } from '@open-wc/testing'
-
-suite('fds-card', () => {
-  test('is defined', () => {
-    const el = document.createElement('fds-card')
-    assert.instanceOf(el, FdsCard)
-  })
-})

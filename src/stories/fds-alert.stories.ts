1afe9fe71aee7ffb8945858ce3001b9f6abd3412
Lilja Hänninen <12098663+lilkettu@users.noreply.github.com>
2023-02-27 07:44:58 +0200

    Icon property for alert component. Added check-circle to icons (#28)

--- src/stories/fds-alert.stories.ts
+++ src/stories/fds-alert.stories.ts
@@ -2,29 +2,35 @@ import { Meta } from '@storybook/web-components'
 import { html, TemplateResult } from 'lit'
 import { FdsAlertVariant } from '../fds-alert'
 import '../fds-icon'
+import { FdsIconType } from '../fds-icon'
 
 export default {
   title: 'Alert',
   args: {
     variant: FdsAlertVariant.error,
+    icon: 'alert-triangle',
   },
   argTypes: {
     variant: {
       options: Object.keys(FdsAlertVariant),
       control: { type: 'select' },
     },
+    icon: {
+      options: ['alert-triangle', 'check-circle', 'alert-circle', undefined],
+      control: { type: 'select' },
+    },
   },
 } as Meta
 
-type Template = (args: { variant: FdsAlertVariant }) => TemplateResult
+type Template = (args: { variant: FdsAlertVariant; icon: FdsIconType }) => TemplateResult
 
-export const Alert: Template = ({ variant }) => {
-  return html`<fds-alert .variant=${variant}>Alert text</fds-alert>`
+export const Alert: Template = ({ variant, icon }) => {
+  return html`<fds-alert .variant=${variant} .icon=${icon}>Alert text</fds-alert>`
 }
 
-export const MultiLineContent: Template = ({ variant }) => {
+export const MultiLineContent: Template = ({ variant, icon }) => {
   return html`
-    <fds-alert .variant=${variant}>
+    <fds-alert .variant=${variant} .icon=${icon}>
       <div>Alert text</div>
       <div>Alert text 2</div>
       <div>Alert text 3</div>

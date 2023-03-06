aee08872f1b9cdd2bbdc00c42e593070942f91a1
Lilja Hänninen <lilja.hanninen@solita.fi>
2023-03-06 08:05:14 +0200

    Docs for alert

--- src/stories/fds-alert.stories.ts
+++ src/stories/fds-alert.stories.ts
@@ -2,22 +2,52 @@ import { Meta } from '@storybook/web-components'
 import { html, TemplateResult } from 'lit'
 import { FdsAlertVariant } from '../fds-alert'
 import '../fds-icon'
+import '../fds-alert'
 import { FdsIconType } from '../fds-icon'
 
 export default {
   title: 'Alert',
+  parameters: {
+    componentSubtitle: 'Message bar for displaying error, info, alert or success messages.',
+    docs: {
+      description: {
+        component: 'Selector: `<fds-alert>`',
+      },
+    },
+  },
   args: {
     variant: FdsAlertVariant.error,
     icon: 'alert-triangle',
+    slot: undefined,
   },
   argTypes: {
     variant: {
-      options: Object.keys(FdsAlertVariant),
-      control: { type: 'select' },
+      options: Object.values(FdsAlertVariant),
+      control: {
+        type: 'select',
+      },
+      description:
+        'Type or color theme for the message. <br><br>\
+        `FdsAlertVariant.error` `FdsAlertVariant.warning` `FdsAlertVariant.info` `FdsAlertVariant.success`',
+      defaultValue: { summary: FdsAlertVariant.error },
+      table: {
+        category: 'Properties',
+      },
     },
     icon: {
       options: ['alert-triangle', 'check-circle', 'alert-circle', undefined],
       control: { type: 'select' },
+      description:
+        'Icon displayed on the left side of the message. Accepts icon name as value. <br><br>\
+      value of `FdsIconType`',
+      defaultValue: { summary: 'undefined' },
+      table: { category: 'Properties' },
+    },
+    slot: {
+      description: 'Default slot',
+      table: { category: 'Slots' },
+      name: '',
+      control: false,
     },
   },
 } as Meta

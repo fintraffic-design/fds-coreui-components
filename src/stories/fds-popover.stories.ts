a9d600375b4eaabbded6ac29055706cb7e7aef2c
Lilja Hänninen <lilja.hanninen@solita.fi>
2023-03-10 08:32:31 +0200

    Change FdsPopoverPosition enums to lowercase. Popover docs

--- src/stories/fds-popover.stories.ts
+++ src/stories/fds-popover.stories.ts
@@ -18,20 +18,71 @@ const colorOptions = {
 
 export default {
   title: 'Popover',
+  parameters: {
+    componentSubtitle: 'Content that is displayed when the user hovers or clicks an element',
+    docs: {
+      description: {
+        component:
+          "`import '@fintraffic-design/coreui-components/src/fds-popover'` <br><br>\
+          Selector: `<fds-popover>`",
+      },
+    },
+  },
   args: {
-    position: FdsPopoverPosition.ABOVE,
-    openOnClick: false,
     popoverText: 'Popover content',
+    position: FdsPopoverPosition.above,
+    openOnClick: false,
     backgroundColor: Object.keys(colorOptions)[0],
+    slotDefault: undefined,
+    slotPopover: undefined,
   },
   argTypes: {
+    popoverText: {
+      description: 'Only for Storybook presentation purposes.',
+    },
     position: {
       options: Object.values(FdsPopoverPosition),
       control: { type: 'select' },
+      description:
+        'Direction the popover opens. <br><br>\
+        `FdsPopoverPosition.above` `FdsPopoverPosition.below` `FdsPopoverPosition.left` `FdsPopoverPosition.right`',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: `'${FdsPopoverPosition.above}'` },
+      },
     },
     backgroundColor: {
       options: Object.keys(colorOptions),
       control: { type: 'select' },
+      description:
+        'Background color for the popover. <br><br>\
+        `FdsColorToken`',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'FdsColorBrandWhite' },
+      },
+    },
+    openOnClick: {
+      description:
+        'Whether the popover opens by clicking the element. <br><br>\
+        `boolean`',
+      table: {
+        category: 'Properties',
+        defaultValue: { summary: 'false' },
+      },
+    },
+    slotDefault: {
+      description:
+        'Default slot. Container for the hoverable/clickable element that is always visible and connected to the popover.',
+      table: { category: 'Slots' },
+      name: '',
+      control: false,
+    },
+    slotPopover: {
+      description: 'Popover slot. Container for the content in popover.',
+      table: { category: 'Slots' },
+      name: 'popover',
+      control: false,
     },
   },
 }
@@ -84,6 +135,14 @@ export const IconPopover: Template = ({ position, openOnClick, popoverText, back
   `
 }
 
+IconPopover.parameters = {
+  docs: {
+    description: {
+      story: 'An example of a popover connected to an icon.',
+    },
+  },
+}
+
 export const PopoverWithHeader: Template = ({ position, openOnClick, popoverText, backgroundColor }) => {
   return html`
     <div style="width: fit-content; margin: 150px auto;">
@@ -108,4 +167,12 @@ export const PopoverWithHeader: Template = ({ position, openOnClick, popoverText
       </fds-popover>
     </div>
   `
+}
+
+PopoverWithHeader.parameters = {
+  docs: {
+    description: {
+      story: 'An example of a large popover with a header.',
+    },
+  },
 }

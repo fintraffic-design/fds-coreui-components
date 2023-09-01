72bf5d983673ebdc594ef35af11433914ca42737
Roope Lindström <roope.lindstrom@solita.fi>
2023-09-01 11:48:02 +0300

    fds-dialog: Add overlay property

--- src/fds-dialog.ts
+++ src/fds-dialog.ts
@@ -1,9 +1,9 @@
 import { css, html, LitElement, PropertyValues } from 'lit'
-import { customElement, property, query } from 'lit/decorators.js'
+import { customElement, property, query, queryAssignedElements, queryAsync } from 'lit/decorators.js'
 import { TemplateResult } from 'lit-html'
 import './fds-card'
 import { tokenVar } from './utils/token-utils'
-import { FdsRadiusLarge, FdsStyleElevation400 } from '@fintraffic-design/coreui-css'
+import { FdsColorBrandBlack, FdsRadiusLarge, FdsStyleElevation400 } from '@fintraffic-design/coreui-css'
 
 /**
  * Dialog component.
@@ -12,13 +12,23 @@ import { FdsRadiusLarge, FdsStyleElevation400 } from '@fintraffic-design/coreui-
  *
  * @property {boolean} modal
  * Dialog is a modal dialog: Does not allow interaction with background elements.
+ *
+ * @property {boolean} overlay
+ * Dialog shows an overlay: Does not allow interaction with the dialog.
  */
 @customElement('fds-dialog')
 export default class FdsDialog extends LitElement {
   @property() modal: boolean = false
+  @property() overlay: boolean = false
 
   @query('dialog')
   private readonly dialog: HTMLDialogElement | undefined
+
+  @queryAssignedElements()
+  private readonly slots: HTMLElement[] | undefined
+
+  @queryAsync('.overlay')
+  private readonly overlayElement: Promise<HTMLDivElement | undefined> | undefined
 
   constructor() {
     super()
@@ -40,14 +50,32 @@ export default class FdsDialog extends LitElement {
         this.dialog?.show()
       }
     }
+
+    this.overlayElement?.then(overlay => {
+      const [slot] = this.slots ?? []
+
+      if (overlay && slot) {
+        const { width, height } = slot.getBoundingClientRect()
+        overlay.style.width = this.overlaySizeStyle(width)
+        overlay.style.height = this.overlaySizeStyle(height)
+        overlay.style.pointerEvents = this.overlay ? 'auto' : 'none'
+        overlay.style.opacity = this.overlay ? '0.5' : '0'
+        slot.inert = this.overlay
+      }
+    })
   }
 
   override render(): TemplateResult {
     return html`
       <dialog style="${this.style.cssText}" @cancel=${(e: Event): void => e.preventDefault()}>
+        <div class="overlay" inert></div>
         <slot></slot>
       </dialog>
     `
+  }
+
+  private overlaySizeStyle(size: number): string {
+    return `calc(${size}px + ${tokenVar(FdsRadiusLarge)})`
   }
 
   static override styles = css`
@@ -58,6 +86,17 @@ export default class FdsDialog extends LitElement {
       padding: calc(${tokenVar(FdsRadiusLarge)} / 2);
       overflow: visible;
       outline: none;
+    }
+
+    .overlay {
+      position: fixed;
+      margin: calc(${tokenVar(FdsRadiusLarge)} / -2);
+      border-radius: ${tokenVar(FdsRadiusLarge)};
+      background-color: ${tokenVar(FdsColorBrandBlack)};
+      pointer-events: none;
+      transition: opacity 0.2s ease;
+      opacity: 0;
+      z-index: 2;
     }
   `
 }

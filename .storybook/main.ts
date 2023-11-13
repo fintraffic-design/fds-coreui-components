c981706967146cf6d0f6f577a0c69ca79ffa60ce
Kari Söderholm <kari.soderholm@solita.fi>
2023-11-13 14:45:01 +0200

    feat: enable Icons gallery in Storybook
    Enable `*.stories.mdx` in Storybook config.
    Comment out Subtitle from Icons story to avoid error: "Error: No primary story defined for docs entry. Did you forget to use `<Meta>`?"
    This error seems to be related to:
    https://github.com/storybookjs/storybook/issues/19964
    https://github.com/storybookjs/storybook/issues/22490

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,9 @@ import type { StorybookConfig } from '@storybook/web-components-vite'
 const config: StorybookConfig = {
   framework: {
     name: '@storybook/web-components-vite',
-    options: {}
+    options: {},
   },
-  stories: ['../src/stories/*.stories.ts'],
+  stories: ['../src/stories/*.stories.@(ts|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   features: {
     buildStoriesJson: true,
